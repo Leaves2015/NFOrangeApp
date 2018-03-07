91d94bbc8f69833b0b58bc8ea85da9d72510cf79
WangJun <1141899305@qq.com>
2018-03-07 22:40:24 +0800

    新增home页面
    新增商城home页面

---
+++ pages/home/home.js
@@ -0,0 +1,78 @@
+// pages/home/home.js
+var json = require("../../data/Home_data.js")
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      main_key:json.homeIndex
+    })
+  },
+
+  btn: function (e) {
+    var homeId = e.currentTarget.dataset.id
+    console.log(homeId)
+    wx.navigateTo({
+      url: '../../pages/home-details/home-details?id=' + homeId,
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
